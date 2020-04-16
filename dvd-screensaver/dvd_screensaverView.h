91145eea299fbc3b9b01f67f96a6c96c576d3f6a
privera <privera@celerity.com>
2020-04-15 23:16:04 -0400

    Initial Commit

---
+++ dvd-screensaver/dvd_screensaverView.h
@@ -0,0 +1,13 @@
+//
+//  dvd_screensaverView.h
+//  dvd-screensaver
+//
+//  Created by Pablo Rivera on 4/15/20.
+//  Copyright © 2020 Pablo Rivera. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface dvd_screensaverView : ScreenSaverView
+
+@end
---
+++ dvd-screensaver/dvd_screensaverView.m
@@ -0,0 +1,52 @@
+//
+//  dvd_screensaverView.m
+//  dvd-screensaver
+//
+//  Created by Pablo Rivera on 4/15/20.
+//  Copyright © 2020 Pablo Rivera. All rights reserved.
+//
+
+#import "dvd_screensaverView.h"
+
+@implementation dvd_screensaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
