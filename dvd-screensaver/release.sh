4128bb763b26ef6b16cd46cf02c6fc5d3b256d49
privera <privera@celerity.com>
2020-04-16 00:18:07 -0400

    added built asset ready to use

---
+++ dvd-screensaver/release.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#  release.sh
+#  dvd-screensaver
+#
+#  Created by Pablo Rivera on 4/16/20.
+#  Copyright © 2020 Pablo Rivera. All rights reserved.
+
+#!/bin/bash
+
+zip -r -X "dvd-screensaver.saver.zip" "dvd-screensaver.saver"
